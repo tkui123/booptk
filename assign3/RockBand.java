a50cc0b1dc8ce4e2521df0a6038e41f81da7954e
Tyler Kui <tkui@stevens.edu>
2017-11-05 16:18:24 -0500

    Almost there!!!

--- assign3/RockBand.java
+++ assign3/RockBand.java
@@ -9,9 +9,105 @@ import cos126.StdAudio;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class RockBand {
+
+    public static void playFromFile(String fileName) throws IOException {
+	String content;
+	boolean songNameFound = false;
+	int speed = 0;
+	boolean bassToggle = false;
+	String note = "";
+
+	Guitar guitar = new Guitar(37);
+	Bass bass = new Bass(37);
+	Piano piano = new Piano(37);
+	Drum drum = new Drum(19);
+	
+	String guitarBassKeyboard = "`1234567890-=qwertyuiop[]\\asdfghjkl;'";
+	String pianoKeyboard = "~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"";
+	String drumKeyboard = "ZXCVBNM<>?zxcvbnm,.";
+
+	content = new String(Files.readAllBytes(Paths.get("Dont_Stop.txt")));
+	//System.out.println(content);
+
+	for (String part : content.split("\\s+")) {
+	    System.out.println(part);
+	    note = part;
+	    
+	    if (!songNameFound) {
+		if (part.substring(0, 2).equals("@@")) {
+		    System.out.println("NOW PLAYING - " + part.substring(2));
+		    songNameFound = true;
+		    continue;
+		}
+	    } else if (part.length() > 1) {
+	        if (part.substring(0, 2).equals("##")) {
+		    speed = Integer.valueOf(part.substring(2));
+		    continue;
+		} else if (part.substring(0, 2).equals("LL")) {
+		    bassToggle = !bassToggle;
+		    note = part.substring(2);
+		}
+	    }
+
+	    for (int i = 0; i < note.length(); i++) {
+		char key = note.charAt(i);
+		//System.out.println(key);
+
+		if (bassToggle) {
+		    if (key == '/') {
+			bassToggle = false;
+			continue;
+		    }
+		}
+			
+		int guitarBassIndex = guitarBassKeyboard.indexOf(key);
+		int pianoIndex = pianoKeyboard.indexOf(key);
+		int drumIndex = drumKeyboard.indexOf(key);
+		if (guitarBassIndex > -1) {
+		    if (!bassToggle) {
+			guitar.playNote(guitarBassIndex);
+		    } else {
+			//System.out.println("Hi");
+			//System.out.println(key);
+			bass.playNote(guitarBassIndex);
+
+		    }
+		}
+		 
+		else if (pianoIndex > -1) { piano.playNote(pianoIndex); }
+
+		else if (drumIndex > -1) { drum.playNote(drumIndex); }
+
+		for (int j = 0; j <= speed; j++) {
+		    StdAudio.play(guitar.ringNotes() + bass.ringNotes() +  piano.ringNotes() + drum.ringNotes());
+		}
+	    }
+	}
+	
+    }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+	System.out.println(args[0]);
+	
+	if (args.length == 2) {
+	    //System.out.println(args[0]);
+	    //System.out.println(args[1]);
+
+	    //System.out.println(args[0].equals("-play_from_file"));
+	    
+	    if (args[0].equals("-play_from_file")) {
+		playFromFile(args[1]);
+		
+	    }
+
+	    return;
+	}
+	
 	Guitar guitar = new Guitar(37);
 	Bass bass = new Bass(37);
 	Piano piano = new Piano(37);
@@ -32,26 +128,28 @@ public class RockBand {
 		     bassToggle = !bassToggle;
 
 		 //System.out.println(bassToggle);
-
-		 index = guitarBassKeyboard.indexOf(key);
-		 if (index > -1) {
+		 
+		 int guitarBassIndex = guitarBassKeyboard.indexOf(key);
+		 int pianoIndex = pianoKeyboard.indexOf(key);
+		 int drumIndex = drumKeyboard.indexOf(key);
+		 if (guitarBassIndex > -1) {
 		     if (!bassToggle) {
-			 guitar.playNote(index);
+			 guitar.playNote(guitarBassIndex);
 		     }
 		     else {
-			 bass.playNote(index);
+			 //System.out.println("Hi");
+			 //System.out.println(key);
+			 bass.playNote(guitarBassIndex);
 
 		     }
 		 }
+		 
+                 else if (pianoIndex > -1) { piano.playNote(pianoIndex); }
 
-		 index = pianoKeyboard.indexOf(key);
-                 if (index > -1) { piano.playNote(index); }
-
-		 index = drumKeyboard.indexOf(key);
-		 if (index > -1) { drum.playNote(index); }
+		 else if (drumIndex > -1) { drum.playNote(drumIndex); }
              }
 
-	     StdAudio.play(guitar.ringNotes() + piano.ringNotes() + drum.ringNotes());
+	     StdAudio.play(guitar.ringNotes() + bass.ringNotes() +  piano.ringNotes() + drum.ringNotes());
 	
 	}
 	
